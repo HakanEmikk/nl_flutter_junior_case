d23c6d61910b49f1c8cc5b5cd57e19e57a091071
hakanemik51 <hkn.emk51@gmail.com>
2025-09-04 17:00:53 +0300

    app colours added

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,34 @@
+import 'dart:ui';
+
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFE50914);
+  static const Color primaryDark = Color(0xFF6F060B);
+  static const Color secondary = Color(0xFF5949E6);
+  // Base renk
+  static const Color baseWhite = Color(0xFFFFFFFF);
+
+  // Tonlama fonksiyonu
+  static Color whiteTone(int percent) {
+    assert(percent >= 0 && percent <= 100, 'Percent must be 0-100');
+
+    // 0 -> siyah, 100 -> saf beyaz
+    int value = (255 * percent / 100).round();
+    return Color.fromARGB(255, value, value, value);
+  }
+
+  // Alert & Status
+  static const Color success = Color(0xFF00C247);
+  static const Color info = Color(0xFF004CE8);
+  static const Color warming = Color(0xFFFFBE16);
+  static const Color error = Color(0xFFF47171);
+  // Others
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  //Gradients
+  static const RadialGradient primaryGradient = RadialGradient(
+    colors: [Color(0xFF3F0306), Color(0xFF090909)],
+    center: AlignmentGeometry.bottomCenter,
+  );
+}
