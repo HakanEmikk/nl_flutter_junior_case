3261e7f3b5119161365a907b4ff1a82059fe982d
hakanemik51 <hkn.emk51@gmail.com>
2025-09-04 20:41:19 +0300

    splash view completed

---
+++ lib/features/splash/view/splash_view.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:jr_case_boilerplate/core/constants/app_colors.dart';
+import 'package:jr_case_boilerplate/core/constants/app_text_styles.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(gradient: AppColors.primaryGradient),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset('assets/images/Icon.png', width: 125, height: 125),
+              SizedBox(height: 16),
+              Text(
+                "Shartflix",
+                style: AppTextStyles.heading2.copyWith(
+                  color: AppColors.baseWhite,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
