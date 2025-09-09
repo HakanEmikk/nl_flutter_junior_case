7defe5bb1203d8dfd7fce56b6266b7793af40e50
hakanemik51 <hkn.emk51@gmail.com>
2025-09-09 14:14:25 +0300

    Added background color support to CustomPrimaryButton

--- lib/core/widgets/buttons/custom_primary_button.dart
+++ lib/core/widgets/buttons/custom_primary_button.dart
@@ -4,17 +4,22 @@ class CustomPrimaryButton extends StatelessWidget {
   const CustomPrimaryButton({
     required this.onPressed,
     required this.child,
+    this.bacgroundColor,
     super.key,
   });
   final void Function()? onPressed;
   final Widget child;
+  final Color? bacgroundColor;
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onPressed,
+
       style: ElevatedButton.styleFrom(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
         elevation: 0,
+
+        backgroundColor: bacgroundColor,
       ),
       child: child,
     );
